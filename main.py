95fce754e74d58b0472c8fa35a24eca29b63c0a9
loheshr-e-x <lohesh.m.2024.cse@rajalakshmi.edu.in>
2026-07-02 20:29:28 +0530

    hidden changes again shockin

--- main.py
+++ main.py
@@ -132,3 +132,9 @@ async def submit_document(validated_utr: str = Form(...), user_roll: str = Form(
             file_object.write(chunk)
 
     return {"status": "Success", "message": "File saved locally for printing."}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
